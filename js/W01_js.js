f9454f9014fbe8cde668577fb90a7858e24f793b
lewisssmith <97068557+lewisssmith19@users.noreply.github.com>
2022-01-17 09:17:54 -0700

    java fix

--- js/W01_js.js
+++ js/W01_js.js
@@ -1,44 +1,51 @@
-function date_ddmmmyy(date)
-{
-  let d = date.getDate();
-  let m = date.getMonth() + 1;
-  let y = date.getYear();
-  if(y >= 2000)
-  {
-    y -= 2000;
-  }
+var d = new Date();
+var months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById('replace-time').innerHTML = days[d.getDay()] + " " + months[d.getMonth()] + " " ;
+document.getElementById('replace-year').innerHTML = d.getFullYear()-3;
+document.getElementById('replace-lastModified').innerHTML = document.lastModified
 
-  if(y >= 100)
-  {
-    y -= 100;
-  }
-  let mmm = 
-    ( 1==m)?'Jan':( 2==m)?'Feb':(3==m)?'Mar':
-    ( 4==m)?'Apr':( 5==m)?'May':(6==m)?'Jun':
-    ( 7==m)?'Jul':( 8==m)?'Aug':(9==m)?'Sep':
-    (10==m)?'Oct':(11==m)?'Nov':'Dec';
+// function date_ddmmmyy(date)
+// {
+//   let d = date.getDate();
+//   let m = date.getMonth() + 1;
+//   let y = date.getYear();
+//   if(y >= 2000)
+//   {
+//     y -= 2000;
+//   }
 
-  return "" +
-    (d<10?"0"+d:d) + "-" +
-    mmm + "-" +
-    (y<10?"0"+y:y);
-}
+//   if(y >= 100)
+//   {
+//     y -= 100;
+//   }
+//   let mmm = 
+//     ( 1==m)?'Jan':( 2==m)?'Feb':(3==m)?'Mar':
+//     ( 4==m)?'Apr':( 5==m)?'May':(6==m)?'Jun':
+//     ( 7==m)?'Jul':( 8==m)?'Aug':(9==m)?'Sep':
+//     (10==m)?'Oct':(11==m)?'Nov':'Dec';
 
-function date_lastmodified()
-{
-  let lmd = document.lastModified;
-  let s   = "Unknown";
-  let d1;
+//   return "" +
+//     (d<10?"0"+d:d) + "-" +
+//     mmm + "-" +
+//     (y<10?"0"+y:y);
+// }
 
-  if(0 != (d1=Date.parse(lmd)))
-  {
-    s = "" + date_ddmmmyy(new Date(d1));
-  }
+// function date_lastmodified()
+// {
+//   let lmd = document.lastModified;
+//   let s   = "Unknown";
+//   let d1;
 
-  return s;
-}
+//   if(0 != (d1=Date.parse(lmd)))
+//   {
+//     s = "" + date_ddmmmyy(new Date(d1));
+//   }
 
-document.write( 
-  "Last Updated " + 
-  date_lastmodified() );
+//   return s;
+// }
+
+// document.write( 
+//   "Last Updated " + 
+//   date_lastmodified() );
 
