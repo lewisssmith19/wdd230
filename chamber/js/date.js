1fb4b34f134db26d4c22184131bd74f3b8dde5a9
lewisssmith <97068557+lewisssmith19@users.noreply.github.com>
2022-01-26 14:26:12 -0700

    lesson4

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -1,16 +1,38 @@
-// select the elements to manipulate (output to)
-const datefield = document.querySelector("date");
-const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+// Todays Current Date
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
-// derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-// long, medium, short options ... try them
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+var d = new Date();
+var todaysWeekDay = days[d.getDay()];
+var todaysDay = d.getDate();
+var todaysMonth = months[d.getMonth()];
+var todaysYear = d.getFullYear();
+
+var fullDate = todaysWeekDay + ", " + todaysMonth + " " + todaysDay + ", " + todaysYear;
+
+console.log(fullDate);
+document.getElementById("currentDate").innerHTML = fullDate;
+   
+$(document).ready(function () {
+            "use strict";
+            // Logo innerhalb des Headers klonen
+            const clonedLogo = $('header .header_logo').clone();
+            // und vor dem input fuer das Hamburger-Menue einfuegen
+            $('nav .menu-container').before(clonedLogo);
+            clonedLogo.addClass("header__shrink");
+            $(window).scroll(function () {
+                // Pruefen, ob nach unten gescrollt wurde
+                if ($(this).scrollTop() > 130) {
+                    $("nav").addClass("stickynav");
+                    // ja: Logo expandieren
+                    clonedLogo.height(80);
+                    clonedLogo.width(80);
+                } else {
+                    $("nav").removeClass("stickynav");
+                    // nein: Logo schrumpfen
+                    clonedLogo.height(0);
+                    clonedLogo.width(0);
+                }
+            })
+        });
