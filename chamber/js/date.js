88b85c3991ca96a8144b2ef697c5a63e5f9de8f2
lewisssmith <97068557+lewisssmith19@users.noreply.github.com>
2022-01-26 22:28:05 -0700

    lesson4

--- chamber/js/date.js
+++ chamber/js/date.js
@@ -14,25 +14,10 @@ var fullDate = todaysWeekDay + ", " + todaysMonth + " " + todaysDay + ", " + tod
 console.log(fullDate);
 document.getElementById("currentDate").innerHTML = fullDate;
    
-$(document).ready(function () {
-            "use strict";
-            // Logo innerhalb des Headers klonen
-            const clonedLogo = $('header .header_logo').clone();
-            // und vor dem input fuer das Hamburger-Menue einfuegen
-            $('nav .menu-container').before(clonedLogo);
-            clonedLogo.addClass("header__shrink");
-            $(window).scroll(function () {
-                // Pruefen, ob nach unten gescrollt wurde
-                if ($(this).scrollTop() > 130) {
-                    $("nav").addClass("stickynav");
-                    // ja: Logo expandieren
-                    clonedLogo.height(80);
-                    clonedLogo.width(80);
-                } else {
-                    $("nav").removeClass("stickynav");
-                    // nein: Logo schrumpfen
-                    clonedLogo.height(0);
-                    clonedLogo.width(0);
-                }
-            })
-        });
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
