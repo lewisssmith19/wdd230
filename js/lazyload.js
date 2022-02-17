ff2febdbd6fb017a855fc230d7ed6a93e6336578
lewisssmith <97068557+lewisssmith19@users.noreply.github.com>
2022-02-17 14:09:10 -0700

    lazyload done

--- js/lazyload.js
+++ js/lazyload.js
@@ -1,31 +1,58 @@
-const images = document.querySelectorAll("[data-src]");
+const targets = document.querySelectorAll('img');
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-        
-    }
+const lazyload = target => {
+    const io = new IntersectionObserver((entries, observer) => {
 
-    img.src = src;
-}
+        entries.forEach(entry => {
+            console.log('test');
 
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 300px 0px"
+            if (entry.isIntersecting) {
+                const img = entry.target;
+                const src = img.getAttribute('data-lazy');
+
+                img.setAttribute('src', src);
+                img.classList.add('fade');
+
+                observer.disconnect();
+            }
+        });
+    });
+
+    io.observe(target);
 };
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
-}, imgOptions);
+targets.forEach(lazyload);
 
-images.forEach(image => {
-    imgObserver.observe(image);
-});
+
+
+// const images = document.querySelectorAll("[data-src]");
+
+// function preloadImage(img) {
+//     const src = img.getAttribute("data-src");
+//     if(!src) {
+//         return;
+        
+//     }
+
+//     img.src = src;
+// }
+
+// const imgOptions = {
+//     threshold: 0,
+//     rootMargin: "0px 0px 300px 0px"
+// };
+
+// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//     entries.forEach(entry => {
+//         if (!entry.isIntersecting) {
+//             return;
+//         } else {
+//             preloadImage(entry.target);
+//             imgObserver.unobserve(entry.target);
+//         }
+//     })
+// }, imgOptions);
+
+// images.forEach(image => {
+//     imgObserver.observe(image);
+// });
