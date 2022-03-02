2486bcaf59552e9c572bd533ead6220932c8f79e
lewisssmith <97068557+lewisssmith19@users.noreply.github.com>
2022-03-01 22:20:17 -0700

    prophets

--- lesson-9/prophets.js
+++ lesson-9/prophets.js
@@ -1,16 +1,19 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
-const cards = document.querySelector('.cards');
+const cards = document.querySelector('.cards'); 
+let prophets = null;
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    prophets = jsonObject.prophets;
+    prophets.forEach(displayProphets);  // temporary checking for valid response and data parsing
   });
 
-  const prophets = jsonObject['prophets'];
-  prophets.forEach(displayProphets);
+  
+  console.log(prophets)
+
   
 
   function displayProphets(prophet) {
@@ -23,9 +26,9 @@ fetch(requestURL)
     h2.textContent = prophet.name + ' ' + prophet.lastname;
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
-    ____.setAttribute('loading', 'lazy');
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
+    portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
