3b008eda8a0648e29ea66d2275366376c042184d
lewisssmith <97068557+lewisssmith19@users.noreply.github.com>
2022-03-05 15:48:05 -0700

    JSSSSSSSSSSSSSSS

--- js/directory.js
+++ js/directory.js
@@ -1,5 +1,5 @@
 const requestURL = 'https://lewisssmith19.github.io/wdd230/js/data.json';
-const companies = document.querySelector('.companies'); 
+let companies = document.querySelector('.companies'); 
 
 fetch(requestURL)
   .then(function (response) {
@@ -11,14 +11,12 @@ fetch(requestURL)
     companys.forEach(displayCompanies);  // temporary checking for valid response and data parsing
   });
   
-  
-  console.log(companies);
 
   
 
-  function displayCompanies(companies) {
+  function displayCompanies(company) {
     // Create elements to add to the document
-    let company = document.createElement('section');
+    let card = document.createElement('section');
     let h2 = document.createElement('h2');
     // let p = document.createElement('p');
     let address = document.createElement('address');
@@ -29,29 +27,29 @@ fetch(requestURL)
   
     // Change the textContent property of the h2 element to contain the prophet's full name
     
-    h2.textContent = companies.name;
-    address.textContent = companies.address; 
-    phone.textContent = companies.phone;
-    a.textContent = companies.website;
+    h2.textContent = company.name;
+    address.textContent = company.address; 
+    phone.textContent = company.phone;
+    a.textContent = company.website;
     a.setAttribute('target', '_blank');
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', companies.image);
+    portrait.setAttribute('src', company.image);
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('alt', 'images');
     portraitContainer.appendChild(portrait);
     portraitContainer.setAttribute('class', 'portraitContainer');
-    company.appendChild(portraitContainer);
+    card.appendChild(portraitContainer);
     // Add/append the section(card) with the h2 element
-    company.appendChild(phone);
+    card.appendChild(phone);
     // company.appendChild(p);
-    company.appendChild(address);
-    company.appendChild(a);
+    card.appendChild(address);
+    card.appendChild(a);
    
 
   
     // Add/append the existing HTML div with the cards class with the section(card)
-    companies.appendChild(company);
+    companies.appendChild(card);
 
     // for (let i=0; i < lastname.length; i++) {
     //     lastname[i] = lastname [i] + "!";
