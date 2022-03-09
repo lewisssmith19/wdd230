b6604470eeb3bce92ec469cde3a8fae33e401ebc
lewisssmith <97068557+lewisssmith19@users.noreply.github.com>
2022-03-09 16:16:19 -0700

    js

--- js/weatherapi.js
+++ js/weatherapi.js
@@ -6,3 +6,14 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
   });
+
+
+  document.querySelector('#current-temp').textContent = jsObject.main.temp;
+
+
+const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+const desc = jsObject.weather[0].description;
+document.querySelector('#icon-src').textContent = iconsrc;
+document.querySelector('#weathericon').setAttribute('src', iconsrc);
+document.querySelector('#weathericon').setAttribute('alt', desc);
+document.querySelector('figcaption').textContent = desc;
