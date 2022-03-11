d13920af153e4a62a6a8aa5f3e01ceab9fb2ffa1
lewisssmith <97068557+lewisssmith19@users.noreply.github.com>
2022-03-11 11:56:33 -0700

    Api

--- js/weatherapi.js
+++ js/weatherapi.js
@@ -4,29 +4,20 @@ const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid
 fetch(apiURL)
   .then((response) => response.json())
   .then((x) => {
-    
     const currentTemp = document.querySelector('#current-temp');
     const weathericon = document.querySelector('#weathericon');
-    console.log(currentTemp)
-    console.log(weathericon)
-    console.log(x.list[0].weather[0].icon)
+    const caption = document.querySelector('figcaption');
 
-
-    
-    currentTemp.textContent = x.main.currentTemp.toFixed(0);
+    currentTemp.textContent = x.list[0].main.temp.toFixed(0);
     let imgsrc = `https://openweathermap.org/img/w/${x.list[0].weather[0].icon}.png`;
-    let imgalt = x.weather[0].description;
+    let imgalt = x.list[0].weather[0].description;
     imgalt = imgalt.split(' ').map(capitalize).join(' ');
-    
-    currentTemp.textContent = x.main.temp.toFixed(0);
-    weathericon.setAttribute('src' , imgsrc);
+
+    weathericon.setAttribute('src', imgsrc);
     weathericon.setAttribute('alt', imgalt);
     caption.innerHTML = imgalt;
-
-    
   });
 
 function capitalize(word) {
-    return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
-};
-
+  return `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+}
