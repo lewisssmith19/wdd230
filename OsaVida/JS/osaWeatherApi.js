c46015b54d9922488e117b5b2ad1da722c452c73
lewisssmith <97068557+lewisssmith19@users.noreply.github.com>
2022-04-05 19:55:22 -0600

    api

--- OsaVida/JS/osaWeatherApi.js
+++ OsaVida/JS/osaWeatherApi.js
@@ -1,4 +1,4 @@
-const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
+const apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={da06ec2c60e15d55813ec4b8ac326745}`;
 fetch(apiURL)
 .then((response) => response.json())
   .then((x) => {
@@ -7,7 +7,7 @@ fetch(apiURL)
     const caption = document.querySelector('figcaption');
 
     currentTemp.textContent = x.list[0].main.temp.toFixed(0);
-    let imgsrc = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
+    let imgsrc = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={da06ec2c60e15d55813ec4b8ac326745}`;
     let imgalt = x.list[0].weather[0].description;
     imgalt = imgalt.split(' ').map(capitalize).join(' ');
 
